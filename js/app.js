8bcddb6110a79ca0cbc591511f3e248a3ff3ef19
giamazonas <skibinskirenee@gmail.com>
2022-02-05 15:50:34 -0500

    getWinner update

--- js/app.js
+++ js/app.js
@@ -8,43 +8,44 @@ let turn
 // in render function this provides logic for what need to display
 
 
-const winCombo = [
-  combo1 = [0, 1, 2],
-  combo2 = [3, 4, 5],
-  combo3 = [6, 7, 8],
-  combo4 = [0, 3, 6],
-  combo5 = [1, 4, 7],
-  combo6 = [2, 5, 8],
-  combo7 = [0, 4, 8],
-  combo8 = [6, 4, 2],
-]
-
-
-
 
 /*---------------------------- Variables (state) ----------------------------*/   //**  1   */ 
 
 //1.1
 let winner = null
+let gameOver 
 // 1.3 
 // let turn 
 //1.2
-//define variables needed to track state of the game
+////define variables needed to track state of the game
 let tie 
+//// let initMessage = `Click your square of choice, to start the game.`
 
 
 /*------------------------ Cached Element References ------------------------*/  //** 2 */
 const sq = document.querySelectorAll('.square')
 const message = document.querySelector('#message')
+
 const gameBoard = document.querySelector('.board')
 
-//   return `Click your square of choice, to start the game.`
-// }
-// allSquares = [sq0 = null, sq1 = null, sq2 = null, sq3 = null, sq4 = null, sq5 = null, sq6 = null, sq7 = null, sq8 = null]
-// console.log(allSquares)
+
+//// allSquares = [sq0 = null, sq1 = null, sq2 = null, sq3 = null, sq4 = null, sq5 = null, sq6 = null, sq7 = null, sq8 = null]
 
 
 
+const winCombo = [
+  combo1 = [0, 1, 2],       //3
+  combo2 = [3, 4, 5],      //12
+  combo3 = [6, 7, 8],     //21
+  combo4 = [0, 3, 6],    //9
+  combo5 = [1, 4, 7],      //12
+  combo6 = [2, 5, 8],     //15
+  combo7 = [0, 4, 8],    //12
+  combo8 = [6, 4, 2],   //12
+]
+
+
+// **  element to display game status??
 //
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -57,66 +58,62 @@ sq.forEach(square => square.addEventListener('click', handleClick))
 // init   --  to initialize stated variablees  //** 3.1 */
 function init(){
   sq = [null, null, null, null, null, null, null, null, null]
-  turn = 1 // 3.2.2 
+  turn = 1   // 1/-1 ? // 3.2.2 
   winner = null // 3.2.3  
   message.textContent = `Let's play! Choose your first square.`
-  render()     //3.2.4 
   sq.innerHTML = ""
+  render()     //3.2.4 
 }
+
 //___________________________________________
 
 //render  -- render these values to the page   //** 3.2 */
-function renderGame() {
+
+function render() {
   gameBoard.forEach((sq, idx) => {
     if (sq === 1){
       sq[idx].innerHTML = 'X'
       sq[idx].style.backgroundColor = 'red';
       message.textContent = `It's O's turn...`
     }
-      else if (sq === -1) {
-        sq[idx].innerHTML = 'O'
-        sq[idx].style.backgrounColor = 'blue';
-        message.textContent = `It's X's turn...`
-      // }else (sq === null){
-      //   sq[idx].innerHTML = ''
-        }
+    else if (sq === -1) {
+      sq[idx].innerHTML = 'O'
+      sq[idx].style.backgrounColor = 'blue';
+      message.textContent = `It's X's turn...`
     }
-  ) 
+    else if (sq === null){
+      sq[idx].innerHTML = ''
+    }
+  }) 
 }
 
-    // i (!isWinner) {
-    //   //indicate whose turn it is
-    // } else if (winner ==='T') {
-    //   //indicate a tie game
-    // }else {
-    //   //congrats to the winner!
-    //   gameMessage.textContent = 'congrats'
-    // }
 
-
-
-function handleClick (event) {
-  console.log(event.target.id)
-}
-
-//play  -  wait for user to click square   //** 3.3 */
-
-
-
-  
-function renderWinner(){
-  let isWinner = winCombo.forEach()
+function getWinner(){
+  let winner = winCombo.forEach()
   for(let c = 0; c < winCombo.length; c++){
-    if (playerX = isWinner) {
+    if (playerX = winner()) {
       return `X is the winner!`
-      console.log(isWinner)
+      console.log(winner())
     // }else if (playerO = isWinner){
       return `O is the winner!`
     }else (tie)=> `Tie Game. Play again!`
     }
   }
+  // (winner() = winCombo.some(arr)
 
-
+  function handleClick(e, idx) {
+    return sq[idx]
+    if (winner !== null) {
+      return gameOver
+  } else return winner.push()
+    return (turn * -1)
+    //obtain index of sq clicked on
+    // extract index from id asigned to element
+    // filter click to an array 
+    // console.log(event.target.id)
+  }
+  
+  //play  -  wait for user to click square   //** 3.3 */
 
     //  handle when use clicks square   //** 5 */
     //  handle player clicking replay button  //**6 */
